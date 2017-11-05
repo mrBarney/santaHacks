4b5a0a111b46c838800096ccfac79693d0ac7816
Jordan Barnfield <jordanbarnfield12@gmail.com>
2017-11-05 01:45:07 -0600

    fixing bullshit

--- website/script/question.js
+++ website/script/question.js
@@ -1,28 +1,23 @@
-$(document).ready(function()
-{
-   var limit = 20;
-   var n = 4;
-   var addB = $(".addB");
-   var questions = $(".Que");
+$(document).ready(function () {
+  var limit = 20;
+  var n = 4;
+  var addB = $(".addB");
+  var questions = $(".Que");
 
-   $( "#datepicker" ).datepicker();
+  $("#datepicker").datepicker();
 
-   $(addB).click(function(e)
- {
-   if(n<limit){
-     $(questions).append('<div class="form-field">  <label>Question '+n+':</label> <input type="text" name="Question '+n+'" class="questions"> <a href="#" class="rem"> X </a> <br><br></div>');
-    n++;
+  $(addB).click(function (e) {
+    if (n < limit) {
+      $(questions).append('<div class="form-field">  <label>Question ' + n + ':</label> <input type="text" name="Question ' + n + '" class="questions"> <a href="#" class="rem"> X </a> <br><br></div>');
+      n++;
     }
- });
+  });
 
- $(questions).on("click", ".rem", function(e){
-   if(n>3){
-   e.preventDefault();
-   $(this).parent('div').remove();
-   n--;
- }
- });
-
-
-
+  $(questions).on("click", ".rem", function (e) {
+    if (n > 0) {
+      e.preventDefault();
+      $(this).parent('div').remove();
+      n--;
+    }
+  });
 });
