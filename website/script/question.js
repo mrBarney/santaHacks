5c1ed78923c9dea27b386c7eeff591e29b7b86a6
cowboy63 <arami.g@outlook.com>
2017-11-04 22:20:57 -0500

    delete all questiona

--- website/script/question.js
+++ website/script/question.js
@@ -10,13 +10,13 @@ $(document).ready(function()
    $(addB).click(function(e)
  {
    if(n<limit){
-     $(questions).append('<div>  <label>Question '+n+':</label> <input type="text" name="question'+n+'" class="questions"> <a href="#" class="rem"> X </a> <br><br></div>');
+     $(questions).append('<div class="form-field">  <label>Question '+n+':</label> <input type="text" name="question'+n+'" class="questions"> <a href="#" class="rem"> X </a> <br><br></div>');
     n++;
     }
  });
 
  $(questions).on("click", ".rem", function(e){
-   if(n>3){
+   if(n>0){
    e.preventDefault();
    $(this).parent('div').remove();
    n--;
