b27103247ec7054356b16e26923203cb01d0b389
Jordan Barnfield <jordanbarnfield12@gmail.com>
2017-11-05 10:34:55 -0600

    match bettered

--- website/script/match.js
+++ website/script/match.js
@@ -1,72 +1,80 @@
 /* The following example list two objects in a bucket. */
 var sourceBucket = "responses.santahacks.com";
+var urlParams = new URLSearchParams(window.location.search);
+var prefixValue = urlParams.get("orgname");
 
 var params = {
   Bucket: sourceBucket,
+  Prefix: prefixValue
 };
+
+var people;
 
 s3.listObjects(params, function (err, data) {
   if (err)
     console.log(err, err.stack); // an error occurred
-  else
+  else {
     console.log(data); // successful response
+    people = data.Contents;
+  }
 });
-
-var people = ["john", "abbie", "bill"];
-
-people = assignSantas(people);
-
-for(var i = 0; i < people.length; i++){
-  console.log(people[i]);
-}
+setTimeout(function () {
+  people = assignSantas(people);
+  for (var i = 0; i < people.length; i++) {
+    console.log(people[i]);
+  }
+}, 3000);
 
 //Begin match
 function assignSantas(array) {
   var matches = [];
 
-  if(!array || !array.length) {
+  if (!array || !array.length) {
     return null;
   }
 
   var santas = array.slice();
   shuffle(santas);
 
-  for(var i=0; i<santas.length; i++) {
+  for (var i = 0; i < santas.length; i++) {
     var santa = santas[i],
-        recipient;
+      recipient;
 
     // Assign santa to the person next to them to avoid assigning to self and avoid duplicate recipients
-    if(i !== santas.length-1) {
-      recipient = santas[i+1];
+    if (i !== santas.length - 1) {
+      recipient = santas[i + 1];
     } else {
-      recipient = santas[0];      
+      recipient = santas[0];
     }
 
-    matches.push({ "santa": santa, "recipient": recipient });
+    matches.push({
+      "santa": santa,
+      "recipient": recipient
+    });
   }
 
   return matches;
 };
 
 function shuffle(array) {
-  var n = array.length, 
-      i, 
-      j;
+  var n = array.length,
+    i,
+    j;
 
-  while(n) {
+  while (n) {
     i = Math.floor(Math.random() * n--);
 
     j = array[n];
     array[n] = array[i];
     array[i] = j;
-  } 
+  }
 }
 
 function parseNames(value) {
   var names = [];
   var vals = value.split('\n');
 
-  for (var i=0; i<vals.length; i++) {
+  for (var i = 0; i < vals.length; i++) {
     var name = vals[i].trim();
     if (name === '') continue;
     names.push(name);
