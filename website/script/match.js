9684e3e5889d506c73636e260a34f4c62ee3e96c
Cooper F. Le <cooperfle@utdallas.edu>
2017-11-05 11:28:25 -0600

    I changed things.

--- website/script/match.js
+++ website/script/match.js
@@ -14,16 +14,19 @@ s3.listObjects(params, function (err, data) {
   if (err)
     console.log(err, err.stack); // an error occurred
   else {
-    console.log(data); // successful response
     people = data.Contents;
   }
 });
 setTimeout(function () {
   people = assignSantas(people);
   for (var i = 0; i < people.length; i++) {
-    console.log(people[i]);
+    var santaKey =  people[i].santa.Key;
+    var elfKey = people[i].recipient.Key;
+    var santa = santaKey.slice(santaKey.indexOf("/")+1, santaKey.indexOf(".json"));
+    var elf = elfKey.slice(elfKey.indexOf("/")+1, elfKey.indexOf(".json"));
+    console.log("Santa: " + santa + " Elf: " + elf);
   }
-}, 3000);
+}, 500);
 
 //Begin match
 function assignSantas(array) {
