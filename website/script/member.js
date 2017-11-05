76a13366d75b9ce30cfe86270724156e13da48d9
Cooper F. Le <cooperfle@utdallas.edu>
2017-11-05 05:31:38 -0600

    Dynamic form generation

--- website/script/member.js
+++ website/script/member.js
@@ -1,12 +1,12 @@
 var config = new AWS.Config({
     accessKeyId: config.key, secretAccessKey: config.secret, region: config.s3region
 });
+const urlParams = new URLSearchParams(window.location.search);
 
 var s3 = new AWS.S3(config);
 const sourceBucket = "setup.santahacks.com";
-const sourceKey = "UTDALLAS.json"; // Filename. Need to get from browser
+const sourceKey = urlParams.get("org") + ".json"; // Filename. Need to get from browser
 const sourceType = "application/json";
-
 AWS.config.update({
     region: config.s3region,
     credentials: new AWS.CognitoIdentityCredentials({
@@ -31,12 +31,67 @@ s3.getObject(params, function(err, data) {
       console.log(new TextDecoder("utf-8").decode(data.Body));
 
       $(org).text(""+(b.Organization));
-
-
-
-
-
-
+      if(b.Question1) {
+        $(question1).text(""+(b.Question1));
+      }
+      else{
+          $(form1).text("");
+      }
+      if(b.Question2) {
+        $(question2).text(""+(b.Question2));
+      }
+      else{
+          $(form2).text("");
+      }
+      if(b.Question3) {
+        $(question3).text(""+(b.Question3));
+      }
+      else{
+          $(form3).text("");
+      }
+      $(question3).text(""+(b.Question3));
+      if(b.Question4) {
+        $(question4).text(""+(b.Question4));
+      }
+      else{
+          $(form4).text("");
+      }
+      if(b.Question5) {
+        $(question5).text(""+(b.Question5));
+      }
+      else{
+          $(form5).text("");
+      }
+      if(b.Question6) {
+        $(question6).text(""+(b.Question6));
+      }
+      else{
+          $(form6).text("");
+      }
+      if(b.Question7) {
+        $(question7).text(""+(b.Question7));
+      }
+      else{
+          $(form7).text("");
+      }
+      if(b.Question8) {
+        $(question8).text(""+(b.Question8));
+      }
+      else{
+          $(form8).text("");
+      }
+      if(b.Question9) {
+        $(question9).text(""+(b.Question9));
+      }
+      else{
+          $(form9).text("");
+      }
+      if(b.Question10) {
+        $(question10).text(""+(b.Question10));
+      }
+      else{
+          $(form10).text("");
+      }
 
       }// successful response
 });
